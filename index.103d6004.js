51e1d2aea5248af3e7d7ee8b581689a9117f2b92
Oleksandr14 <96308880+Oleksandr14@users.noreply.github.com>
2023-02-02 18:50:20 +0000

    Deploying to gh-pages from @ Oleksandr14/cakes-project@b260c4a327a1c71e1e1aad9317212d16dcaf7a60 🚀

---
+++ index.103d6004.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".nav__toggle"),t=document.querySelector(".menu-icon"),c=document.querySelector(".header-wrapper");e.addEventListener("click",(()=>{c.classList.toggle("nav-mobile"),t.classList.toggle("menu-icon-active")}));
+//# sourceMappingURL=index.103d6004.js.map
---
+++ index.14c8a264.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".nav__toggle"),t=document.querySelector(".menu-icon"),c=document.querySelector(".header-wrapper");e.addEventListener("click",(function(){c.classList.toggle("nav-mobile"),t.classList.toggle("menu-icon-active")}))}();
+//# sourceMappingURL=index.14c8a264.js.map
