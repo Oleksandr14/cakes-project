f97f213007166205633130e1f1ad779fa6893aef
Oleksandr Palamarchuk <palamarchuk.oa93@gmail.com>
2023-02-08 14:47:19 +0200

    added scroll-up

--- src/index.js
+++ src/index.js
@@ -8,3 +8,25 @@ navBtn.addEventListener('click', () => {
   headerWrapper.classList.toggle('nav-mobile');
   menuIcon.classList.toggle('menu-icon-active');
 });
+
+// scroll to the top
+
+const offset = 100;
+const scrollUp = document.querySelector('.scroll-up');
+
+const getTop = () => window.pageYOffset || document.documentElement.scrollTop;
+
+window.addEventListener('scroll', () => {
+  if (getTop() > offset) {
+    scrollUp.classList.add('scroll-up--active');
+  } else {
+    scrollUp.classList.remove('scroll-up--active');
+  }
+});
+
+scrollUp.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
